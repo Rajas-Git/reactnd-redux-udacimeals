0cadaeef8be125550c52e84ab5997ee00f3fabf5
Rajus Dhoble <radhoble@microsoft.com>
2017-09-03 17:54:49 -0700

    add combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 import {
     ADD_RECIPE,
     REMOVE_FROM_CALENDAR
@@ -90,4 +92,7 @@ function calendar (state = initialCalendarState, action) {
 }
 
 
-export default calendar;
+export default combineReducers({
+    food,
+    calendar
+});
